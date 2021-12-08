ad350020467ed1c0ef74a798e5d37d0e0adcf174
Greg <gregwenshell@gmail.com>
2021-12-08 13:12:42 +0100

    Init

---
+++ stand.py
@@ -0,0 +1,316 @@
+stand = '''
+5,00    Mieke Geit
+5,00    Flegmatieke Farizeeërs
+5,00    De Gulden Kiekens
+5,00    De Luchtwafels
+5,00    De Loeiende Koe
+5,00    De Terrasridders
+4,94    Profis
+4,92    Donderballers
+4,92    Fironics
+4,92    Gastons Villa
+4,90    Blauwput Omnisport Legends
+4,88    VK Linge
+4,88    ZVC WINA
+4,88    FC Boeziness
+4,86    ZVC Joow
+4,83    Voluit
+4,83    De Marcellekes
+4,83    De VoetbalVrouwen
+4,83    Leuven Biers
+4,83    Old School WTM
+4,83    Patro Hijsen
+4,83    Real Patat
+4,83    Wodka RB Lijpzig
+4,83    Maccabi Tilly-Aviv
+4,83    De Deugnieten
+4,80    FC Football Club
+4,80    The Doodle Wanderers
+4,80    The Buster Picklers
+4,80    WTM A
+4,80    Young Devils
+4,79    OHLeugen
+4,79    Sfeer & Gezelligheid
+4,79    Mokka Juniors
+4,75    AVF
+4,75    FC José
+4,75    FC Wasdekans
+4,75    Eclips
+4,75    Romero
+4,75    AA Phint van Koriander
+4,75    BCO United
+4,75    ZVC Pannalympiakos
+4,75    FC Perto
+4,75    FC Pertdos
+4,75    FC Tzatics
+4,75    De Foempers
+4,75    Sokker Punch
+4,75    LSC
+4,75    Sporting Bonifanzios
+4,71    ZVC Heverlee United
+4,70    Kinderen van God
+4,70    Football Club FC
+4,70    Klimop
+4,70    Blauwput Omnisport Mixed
+4,70    Hak Strak
+4,70    FC Jebus
+4,70    Unicorn United
+4,70    Washizel United
+4,70    VKOO Lazio Coma
+4,70    ZVC Gappies
+4,69    Team Ploeg
+4,69    Young Boys Boutersem
+4,67    RES Interieurs
+4,67    Vitória Diabolix
+4,67    Freestyle
+4,67    Hangover 97
+4,67    FC 't Pleintje
+4,67    Restaurant Arenberg
+4,67    ZV Aan Den Eik
+4,67    Squadra De Pichichi
+4,64    Het Concilie
+4,63    FC Sebieja
+4,63    N.I.T.
+4,63    FC Deli
+4,63    Buena Vista FC
+4,63    ZVC Rode Veters
+4,63    FC Bretel
+4,63    AS Terbank
+4,61    Dètteg Twinteg
+4,60    Daring Leuven B
+4,60    Spartak Leuven
+4,60    FC Langen Bal
+4,60    Winnie en Zonen
+4,60    Patrick Stars
+4,60    Mena Ants
+4,60    MDPD FC
+4,58    Futtosaru
+4,58    FC Meerdaal
+4,58    Schwalbe 04
+4,58    GingerTube
+4,58    Leuven Globeshotters
+4,58    FC Escalé
+4,58    FC De Sjotters
+4,57    Okapi Leuven
+4,57    Stade Louvaniste
+4,50    Leuven Central B
+4,50    FC Bitox
+4,50    FC Selltik Rangers
+4,50    TNT
+4,50    De Gouden Kuikens
+4,50    Plein FC
+4,50    Sambapieten
+4,50    FC The Internationals
+4,50    Bierse Toogzonen
+4,50    FC Oneliner
+4,50    Red Bull Lijpzig
+4,50    Dag Moeder
+4,50    LeefdaalBulls
+4,50    Lokomotiv Kessel-Lo
+4,50    FC Biercelona
+4,50    Regoj de Hejmo
+4,50    Vossem United
+4,50    Partizan Leuven
+4,50    Stormarenden
+4,50    Blauwput Kessel Lo
+4,50    ZVC Rakett
+4,50    FC De Johnnyboys
+4,50    Nadorst
+4,50    Santa Maradona
+4,50    Sporting Olsenbaaik
+4,44    Young Boys Rotselaar
+4,44    OH Gel
+4,43    VK Gercy
+4,43    Bar ZVC
+4,42    Veu t' Geniet FC
+4,42    VC Brabant
+4,42    Spartak Farao
+4,42    Partizan Holsbeek
+4,42    SK Pintelierse
+4,40    Blauwput Omnisport Originals
+4,40    FC De Molotov
+4,40    De Brug
+4,40    Atletico Leuven
+4,40    Dynamo Commerce
+4,40    De Kleine Prins(es)
+4,40    Sporting Nandje
+4,40    De Draperiers
+4,40    Wiz Futsal Leuven
+4,38    Allianza Leuven
+4,38    Das Weltmeister
+4,38    Den Toog Opvelp C
+4,38    Red Plino
+4,36    KK Nozems
+4,36    De Ongure Elementen
+4,36    Bonsai
+4,33    Maradona Kebab
+4,33    ZVC Stalen Tip
+4,33    België
+4,33    Zenith Sint-Snokkersburg
+4,33    Datafoots
+4,31    ZVC Lazarus Roma
+4,30    Starfield
+4,30    Lukania-Express
+4,30    GeEk FC
+4,30    Inter Leuven
+4,30    Poké(Ballers)
+4,29    FC Brakcelona
+4,29    Mossbusters FC
+4,29    Patrick Elentrik
+4,29    Boys on Tour
+4,29    La Mierda
+4,29    Inglorious Ballers
+4,29    Het Rozenhof
+4,25    Atletico Lovana
+4,25    ZVC Floreal
+4,25    Black Balls
+4,25    De Xtras
+4,25    t Excuus
+4,25    ZVC Het Pleintje
+4,25    Poco Loco
+4,25    Sporting Gallapos
+4,25    Stade Hiperlie
+4,25    De Goudhaantjes
+4,21    De Matige Bokskes
+4,21    Deportivo La Corona
+4,21    ZVC Dorstmund
+4,21    DMVL 2
+4,20    Stella United
+4,20    Green Tigers
+4,20    ii De Menne
+4,20    Racing Cara
+4,20    FC Imec
+4,20    FC Schweinings
+4,20    Lokomotieven
+4,20    ZVC De Copains
+4,20    Caradona '86
+4,20    Scouts Wesiuumaal
+4,17    RES Internazionale
+4,17    FC GHB
+4,17    Espérance Leuven'
+4,17    FC KP
+4,17    Lokomotiv Leuven
+4,17    Jaipur
+4,14    Te Goed Voor PSV
+4,14    RSCLA
+4,14    ZVC OX
+4,14    FC Hoorn
+4,14    FC Blessé
+4,13    FC Wolves
+4,13    l3OOl3S
+4,13    RSC Indiaan
+4,13    Tis Gezeimel
+4,13    FC Matig
+4,13    FC Whale
+4,10    Leuven Cardinals Vets
+4,10    KV Stechelen
+4,10    ZVC Derailleurs
+4,10    FC Kasteelmeisters
+4,10    De Shotmadam
+4,08    ZVC Daanballers
+4,07    Beverly Pils
+4,06    FC Melangé
+4,00    Royal Sporting FC VV City
+4,00    BHCM
+4,00    FC Japers
+4,00    ZVC Tien Taarten
+4,00    FC Zjat
+4,00    ZVC Paljax
+4,00    ZVC Ambiorix Leuven
+4,00    FC Den Bolle
+4,00    ZVK Hermes
+4,00    Tottenhesp Hotboyz
+4,00    Pussy's van op den Berg
+4,00    De Gatenvullers
+3,94    Brazzers United
+3,92    De Pittige Poema's
+3,92    FC Patatopdelat
+3,90    t Strooien Dorp
+3,88    Delta Force One
+3,88    Dipi FC
+3,88    Foetsoul
+3,88    FC Skurft
+3,88    Waaiberg Leuven
+3,83    Sammeke Goalies
+3,83    De Manische Kwinkslagers
+3,80    Bierussia Dortmund
+3,75    FC Foute Vrienden
+3,71    Pannathinaikos
+3,71    Espérance Leuven
+3,70    Leuven Cardinals
+3,70    EAGLEs del Spuye
+3,70    ZVC Tigers
+3,67    Ballieboys
+3,67    Leuven Central A
+3,67    Shuta Jamba
+3,67    De Blue Balls
+3,64    Ballon d'Or
+3,64    FC Baasel
+3,63    Rapid Loco
+3,60    De Pijlstormvogels
+3,58    Olijke Schoppers FC
+3,50    AA Mannedag
+3,50    Rode Ster Vlierbeek
+3,50    Rode Ridders
+3,42    Bal Special
+3,42    Donck 06
+3,40    Rotselaar Legends
+3,30    KU Leuven Futsal Team
+3,29    FC De Kapoentjes
+3,25    Den Toog Opvelp D
+3,13    FC Den Chickaway
+3,10    De E Sporters
+3,00    BATE Borokov
+2,90    FFF
+2,75    Inter-Leuven
+2,67    FC The Callballs
+'''
+import re
+
+
+# lijst = re.findall("[^0-9,]+", stand)
+# lijst2 = list(map( lambda x: x.strip() , lijst))
+# print(lijst2.index("FC Oneliner"))
+# print(len(lijst2))
+
+
+class Solution:
+    def __init__(self):
+        pass
+
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        if (len(s) < 2 or len(s) < 2): return s == t
+        if s[-1] == '#':
+            if (len(s) > 2 and s[-2] == '#') or len(s) == 1:
+                s = s[:-1]
+            else:
+                s = s[:-2]
+        if t[-1] == '#':
+            if (len(t) > 2 and t[-2] == '#') or len(t) == 1:
+                t = t[:-1]
+            else:
+                t = t[:-2]
+        return self.backspaceCompare(s[:-1], t[:-1]) and s[-1] == t[-1]
+
+
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+
+REVERSE_MORSE = {value:key for key, value in MORSE_CODE_DICT.items()}
+print(REVERSE_MORSE)
+
+
+
